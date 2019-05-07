4f3597ec0d5c22c944bd0ed10f560f136b0d86cf
andy <dreamrrrr@hotmail.com>
2019-05-07 14:04:13 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.txt_express = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(714, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_ClickAsync);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(0, 96);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(458, 52);
+            this.textBox2.TabIndex = 12;
+            // 
+            // txt_url
+            // 
+            this.txt_url.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_url.Location = new System.Drawing.Point(124, 12);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(322, 25);
+            this.txt_url.TabIndex = 13;
+            this.txt_url.Text = "https://forum.gamer.com.tw/C.php?bsn=23805&snA=618919&tnum=4";
+            // 
+            // txt_express
+            // 
+            this.txt_express.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_express.Location = new System.Drawing.Point(124, 43);
+            this.txt_express.Name = "txt_express";
+            this.txt_express.Size = new System.Drawing.Size(322, 25);
+            this.txt_express.TabIndex = 14;
+            this.txt_express.Text = ".c-article__content";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 148);
+            this.Controls.Add(this.txt_express);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "爬蟲測試";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.TextBox txt_express;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using PuppeteerSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("http://www.google.com");
+
+            var outputFile = Application.StartupPath + "/Download/a.test.pdf";
+
+            // 截圖
+            //await page.ScreenshotAsync();
+
+            // 產PDF
+            //await page.PdfAsync(outputFile);
+
+        }
+
+        private async void button2_ClickAsync(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("測試訊息！！！Form1！！！");
+            var turl = txt_url.Text;
+            var express = $"Array.from(document.querySelectorAll('{txt_express.Text}')).map(a => a.innerText);";
+            textBox2.Text = "";
+            var launchOptions = new LaunchOptions()
+            {
+                Headless = true,
+                Timeout = 1000000
+            };
+            var ops = new NavigationOptions()
+            {
+                Timeout = 120000,
+            };
+            using (var browser = await Puppeteer.LaunchAsync(launchOptions))
+            using (var page = await browser.NewPageAsync())
+            {
+                //await page.GoToAsync("https://github.com/kblok/puppeteer-sharp");
+                //var element = await page.QuerySelectorAsync("h1");
+                //var innerHtml = await element.GetPropertyAsync("innerHTML").Result.JsonValueAsync<string>();
+                //textBox2.Text = innerHtml;
+
+                //var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('a')).map(a => a.href);";
+                //var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('.markdown-body'));";
+                //var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
+                //foreach (string url in urls)
+                //{
+                //    textBox2.Text += $"Url: {url} {Environment.NewLine}";
+                //}
+
+                await page.GoToAsync(turl, ops);
+                var artist = await page.EvaluateExpressionAsync<string[]>(express);
+                var num = 1;
+                foreach (string url in artist)
+                {
+                    //var content = $"{num++} :  {url.Replace("\n", "\r\n")}{Environment.NewLine}";
+                    var content = $"{num++} :  {url.Replace("\n", "\r\n")}";
+                    textBox2.Text += content;
+                    Debug.Print(content);
+                }
+
+            }  
+        }      
+    }          
+
+}
