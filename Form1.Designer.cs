8da15e436711979103379e8965a43ef1db293226
andy <dreamrrrr@hotmail.com>
2019-05-13 17:38:09 +0800

    add baha comment

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.txt_url = new System.Windows.Forms.TextBox();
             this.txt_express = new System.Windows.Forms.TextBox();
+            this.txt_express2 = new System.Windows.Forms.TextBox();
+            this.txt_url2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txt_express3 = new System.Windows.Forms.TextBox();
+            this.txt_url3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -60,13 +66,13 @@
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(0, 96);
+            this.textBox2.Location = new System.Drawing.Point(0, 208);
             this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(458, 52);
+            this.textBox2.Size = new System.Drawing.Size(458, 54);
             this.textBox2.TabIndex = 12;
             // 
             // txt_url
@@ -78,7 +84,7 @@
             this.txt_url.Name = "txt_url";
             this.txt_url.Size = new System.Drawing.Size(322, 25);
             this.txt_url.TabIndex = 13;
-            this.txt_url.Text = "https://forum.gamer.com.tw/C.php?bsn=23805&snA=618919&tnum=4";
+            this.txt_url.Text = "https://forum.gamer.com.tw/B.php?bsn=23805&subbsn=0";
             // 
             // txt_express
             // 
@@ -89,13 +95,83 @@
             this.txt_express.Name = "txt_express";
             this.txt_express.Size = new System.Drawing.Size(322, 25);
             this.txt_express.TabIndex = 14;
-            this.txt_express.Text = ".c-article__content";
+            this.txt_express.Text = ".b-list__tile";
+            // 
+            // txt_express2
+            // 
+            this.txt_express2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_express2.Location = new System.Drawing.Point(124, 105);
+            this.txt_express2.Name = "txt_express2";
+            this.txt_express2.Size = new System.Drawing.Size(322, 25);
+            this.txt_express2.TabIndex = 17;
+            this.txt_express2.Text = ".c-article__content";
+            // 
+            // txt_url2
+            // 
+            this.txt_url2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_url2.Location = new System.Drawing.Point(124, 74);
+            this.txt_url2.Name = "txt_url2";
+            this.txt_url2.Size = new System.Drawing.Size(322, 25);
+            this.txt_url2.TabIndex = 16;
+            this.txt_url2.Text = "https://forum.gamer.com.tw/C.php?bsn=23805&snA=619486&tnum=19";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 56);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txt_express3
+            // 
+            this.txt_express3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_express3.Location = new System.Drawing.Point(124, 167);
+            this.txt_express3.Name = "txt_express3";
+            this.txt_express3.Size = new System.Drawing.Size(322, 25);
+            this.txt_express3.TabIndex = 20;
+            this.txt_express3.Text = ".reply-content__article";
+            // 
+            // txt_url3
+            // 
+            this.txt_url3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_url3.Location = new System.Drawing.Point(124, 136);
+            this.txt_url3.Name = "txt_url3";
+            this.txt_url3.Size = new System.Drawing.Size(322, 25);
+            this.txt_url3.TabIndex = 19;
+            this.txt_url3.Text = "23805, 3650095";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 56);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 148);
+            this.ClientSize = new System.Drawing.Size(458, 262);
+            this.Controls.Add(this.txt_express3);
+            this.Controls.Add(this.txt_url3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txt_express2);
+            this.Controls.Add(this.txt_url2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.txt_express);
             this.Controls.Add(this.txt_url);
             this.Controls.Add(this.textBox2);
@@ -115,6 +191,12 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox txt_url;
         private System.Windows.Forms.TextBox txt_express;
+        private System.Windows.Forms.TextBox txt_express2;
+        private System.Windows.Forms.TextBox txt_url2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txt_express3;
+        private System.Windows.Forms.TextBox txt_url3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using PuppeteerSharp;
+﻿using Newtonsoft.Json;
+using PuppeteerSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +7,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -44,6 +46,61 @@ namespace PForm
             //System.Diagnostics.Debug.WriteLine("測試訊息！！！Form1！！！");
             var turl = txt_url.Text;
             var express = $"Array.from(document.querySelectorAll('{txt_express.Text}')).map(a => a.innerText);";
+            await NewMethod(turl, express);
+
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("測試訊息！！！Form1！！！");
+            var turl = txt_url2.Text;
+            var express = $"Array.from(document.querySelectorAll('{txt_express2.Text}')).map(a => a.innerText);";
+            await NewMethod(turl, express);
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("測試訊息！！！Form1！！！");
+            var strAry = txt_url3.Text.Split(',').Select(c => c.Trim()).ToList();
+
+            var turl = $"https://forum.gamer.com.tw/ajax/moreCommend.php?bsn={strAry[0]}&snB={strAry[1]}&returnHtml=1";
+
+            // 建立 webclient
+            using (WebClient webClient = new WebClient())
+            {
+                // 指定 WebClient 的編碼
+                webClient.Encoding = Encoding.UTF8;
+                // 指定 WebClient 的 Content-Type header
+                webClient.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                // 從網路 url 上取得資料
+                var body = webClient.DownloadString(turl);
+                var jsonObj = JsonConvert.DeserializeObject<Rootobject>(body);
+
+                HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+                foreach (var item in jsonObj.html)
+                {
+                    htmlDoc.LoadHtml(item);
+                    var inputs = from input in htmlDoc.DocumentNode.Descendants("article")
+                                 select input;
+
+                    foreach (var input in inputs)
+                    {
+                        textBox2.Text += input.InnerText;
+                        Debug.Print(input.InnerText);
+                    }
+                }
+            }
+        }
+
+        public class Rootobject
+        {
+            public int next_snC { get; set; }
+            public string[] html { get; set; }
+        }
+
+        private async Task NewMethod(string turl, string express)
+        {
             textBox2.Text = "";
             var launchOptions = new LaunchOptions()
             {
@@ -57,32 +114,20 @@ namespace PForm
             using (var browser = await Puppeteer.LaunchAsync(launchOptions))
             using (var page = await browser.NewPageAsync())
             {
-                //await page.GoToAsync("https://github.com/kblok/puppeteer-sharp");
-                //var element = await page.QuerySelectorAsync("h1");
-                //var innerHtml = await element.GetPropertyAsync("innerHTML").Result.JsonValueAsync<string>();
-                //textBox2.Text = innerHtml;
-
-                //var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('a')).map(a => a.href);";
-                //var jsSelectAllAnchors = @"Array.from(document.querySelectorAll('.markdown-body'));";
-                //var urls = await page.EvaluateExpressionAsync<string[]>(jsSelectAllAnchors);
-                //foreach (string url in urls)
-                //{
-                //    textBox2.Text += $"Url: {url} {Environment.NewLine}";
-                //}
 
                 await page.GoToAsync(turl, ops);
                 var artist = await page.EvaluateExpressionAsync<string[]>(express);
                 var num = 1;
                 foreach (string url in artist)
                 {
-                    //var content = $"{num++} :  {url.Replace("\n", "\r\n")}{Environment.NewLine}";
-                    var content = $"{num++} :  {url.Replace("\n", "\r\n")}";
+                    var content = $"{num++} :  {url.Replace("\n", "\r\n")}{Environment.NewLine}";
+                    //var content = $"{num++} :  {url.Replace("\n", "\r\n")}";
                     textBox2.Text += content;
                     Debug.Print(content);
                 }
 
-            }  
-        }      
+            }
+        }
     }          
 
 }
